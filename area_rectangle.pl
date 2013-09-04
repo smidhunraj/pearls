f22205b856e371c237d419069f8a7bf01a4034ad
smidhunraj <smidhunraj@gmail.com>
2013-09-04 11:43:23 +0530

    First commit

---
+++ area_rectangle.pl
@@ -0,0 +1,14 @@
+#Area and perimeter  of a rectangle
+#!bin/usr/perl
+print "Enter the length of the rectangle";
+$length=<>;
+print "Enter the breadth of the rectangle";
+$breadth=<>;
+print "\nArea of the rectangle";
+$area=$length *$breadth;
+print " $area";
+print "\nPerimeter of the recatngle ";
+$perimeter=2*($length+$breadth);
+print "$perimeter\n ";
+exit;
+
