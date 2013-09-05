54d3f78a8e1c3b10f10749e4f8b01b4a664a0a23
smidhunraj <smidhunraj@gmail.com>
2013-09-05 15:03:24 +0530

    First commit

---
+++ greater.pl
@@ -0,0 +1,11 @@
+#greatest of two numbers....
+#!usr/bin/perl
+print "Enter the first number";
+$num1=<>;
+print "Enter the the second number";
+$num2=<>;
+if($num1>$num2)
+{ print " greatest number is $num1";}
+else
+ {print " greatest number is $num2";}
+exit;
